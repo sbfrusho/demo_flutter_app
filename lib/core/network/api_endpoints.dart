c5e00c6fe6081626b6d63b5ff25a7e92ca6100bd
sbfrusho <rushocseru28@gmail.com>
2025-03-09 21:37:45 +0600

    api endpoint setup
    api endpoint successfully set up

---
+++ lib/core/network/api_endpoints.dart
@@ -0,0 +1,5 @@
+class ApiEndpoints {
+  static const String baseUrl = 'https://reqres.in/api';
+  static const String login = '$baseUrl/login';
+  static const String signUp = '$baseUrl/register';
+}
